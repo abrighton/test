4bafc358053a57d158e378b26e1af0f59ab73d20
Allan Brighton <Allan.Brighton@t-online.de>
2013-07-09 23:31:35 +0200

    Added requests/sec to output

--- thrift/src/main/java/test/JavaClient.java
+++ thrift/src/main/java/test/JavaClient.java
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package test;
 
 // Generated code
@@ -31,25 +12,27 @@ import org.apache.thrift.protocol.TProtocol;
 
 public class JavaClient {
     public static void main(String[] args) {
-        if (args.length != 4) {
-            System.out.println("Expected 4 args: host port count blobSize");
+        if (args.length != 3) {
+            System.out.println("Expected 3 args: host port count");
             System.exit(1);
         }
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+        int count = Integer.valueOf(args[2]);
+        TTransport transport = new TSocket(host, port);
         try {
-            String host = args[0];
-            int port = Integer.valueOf(args[1]);
-            int count = Integer.valueOf(args[2]);
-            int size = Integer.valueOf(args[3]);
-
-            TTransport transport = new TSocket(host, port);
             transport.open();
-
             TProtocol protocol = new TBinaryProtocol(transport);
             BinaryService.Client client = new BinaryService.Client(protocol);
 
-            perform(client, count, size, false);
-            perform(client, count, size, true);
-
+            perform(client, count, 32, false);
+            int[] ar = new int[]{32, 64, 256, 1024};
+            long t = System.currentTimeMillis();
+            for (int size : ar) {
+                perform(client, count, size, true);
+            }
+            double secs = (System.currentTimeMillis() - t) / 1000.0;
+            System.out.println((count*ar.length/secs) + " Total requests/sec");
             transport.close();
         } catch (TException x) {
             x.printStackTrace();
@@ -64,7 +47,7 @@ public class JavaClient {
         if (print) {
             double secs = (System.currentTimeMillis() - t) / 1000.0;
             System.out.println("Transfered " + count + " " + size + " byte blobs in " + secs + " seconds = "
-                + (count*size/secs) + " bytes/sec");
+                    + (count * size / secs) + " bytes/sec, " + (count / secs) + " requests/sec");
         }
     }
 }
